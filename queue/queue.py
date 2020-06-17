60a6bb41aa029ff6d6b2faf44595ea64fc225f21
Daniel Martin <danielmartin1205@gmail.com>
2020-06-17 19:47:16 -0400

    day 1 MVP

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,62 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+#         self.size +=1
+
+#     def dequeue(self):
+#         if self.size > 0:
+#             item = self.storage.pop(0)
+#             self.size -=1
+#             return item
+#         else:
+#             return None
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.head = None
+        self.tail = None
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        newNode = Node(value)
+        self.size +=1
+
+        if not self.head and not self.tail:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next_node = newNode
+            self.tail = newNode
 
     def dequeue(self):
-        pass
+        if not self.head:
+            return None
+        elif self.head.next_node is None:
+            self.size -= 1
+            item = self.head.value
+            self.head = None
+            self.tail = None
+            return item
+        else:
+            self.size -= 1
+            item = self.head.value
+            self.head = self.head.next_node
+            return item
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,70 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.size+=1
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) > 0:
+#             self.size -=1
+#             item = self.storage.pop(-1)
+#             return item
+#         else:
+#             return None
+
+
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
 class Stack:
+
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.tail = None
+        self.head = None
 
     def __len__(self):
-        pass
-
+        return self.size
+    
     def push(self, value):
-        pass
+        newNode = Node(value)
+        self.size +=1
 
+        if not self.head and not self.tail:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+    
     def pop(self):
-        pass
+        if not self.head:
+            return None
+        elif self.head.next is None:
+            self.size -=1
+            item = self.head.value
+            self.head = None
+            return item
+        else:
+            self.size -=1
+            item = self.tail.value
+            cur = self.head
+
+            while cur.next != None:
+                if cur.next == self.tail:
+                    self.tail = cur
+                    cur.next = None
+                    break
+                cur = cur.next
+            return item
